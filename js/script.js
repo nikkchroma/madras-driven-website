4cfa0b85a0e95be3bcc3f259fc843d74e7555ede
nikkchroma <nikhil.m.13.2004@gmail.com>
2026-03-09 08:16:07 +0530

    Add loader fade-out functionality
    Implement loader fade-out effect on page load.

---
+++ js/script.js
@@ -0,0 +1,19 @@
+console.log("Madras Driven site ready");
+
+/* loader fade out */
+
+window.addEventListener("load", () => {
+
+const loader = document.getElementById("loader");
+
+setTimeout(() => {
+
+loader.style.opacity = "0";
+
+setTimeout(() => {
+loader.style.display = "none";
+}, 600);
+
+}, 1800);
+
+});
