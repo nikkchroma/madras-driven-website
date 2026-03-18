ec265fb24fbfbdbaaab4d66142fc26bbfb887b1f
nikkchroma <nikhil.m.13.2004@gmail.com>
2026-03-18 19:41:57 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,4 +14,16 @@ setTimeout(function(){
 
 }, 1200);
 
+window.addEventListener("load", function(){
+
+const loader = document.getElementById("loader");
+
+loader.style.opacity = "0";
+
+setTimeout(function(){
+  loader.style.display = "none";
+}, 600);
+
+});
+
 });
